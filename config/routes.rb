3f1bb5f944e364ade25d9647100c2c92b6a84cd3
y-tsujikawa <184855547+y-tsujikawa@users.noreply.github.com>
2024-11-01 11:34:28 +0000

    [Add] bookのdestroy機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  # get 'books' => 'books#new'
-  get 'books' => 'books#index'
-  get 'books' => 'books#new'
-  post 'books' => 'books#create'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
   get '/' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books
+  # get 'books' => 'books#new'
+  # get 'books' => 'books#index'
+  # get 'books' => 'books#new'
+  # post 'books' => 'books#create'
+  # get 'books/:id' => 'books#show', as: 'book'
+  # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  # patch 'books/:id' => 'books#update', as: 'update_book'
+  # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
