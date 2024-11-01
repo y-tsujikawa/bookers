2f97831119946f9ffb2307d8c007dd74810b6e7e
y-tsujikawa <184855547+y-tsujikawa@users.noreply.github.com>
2024-11-01 12:11:38 +0000

    [Add] レイアウト/デザインの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
+  # def new
+  #   @book = Book.new
+  # end
 
   def create
     book = Book.new(book_params)
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
   resources :books
-  # get 'books' => 'books#new'
   # get 'books' => 'books#index'
-  # get 'books' => 'books#new'
   # post 'books' => 'books#create'
   # get 'books/:id' => 'books#show', as: 'book'
   # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
