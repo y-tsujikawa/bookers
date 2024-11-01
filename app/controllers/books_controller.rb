dbbc6a6144ef90bd696273f8a63e2c833b111bd6
y-tsujikawa <184855547+y-tsujikawa@users.noreply.github.com>
2024-11-01 15:01:23 +0000

    [Add] updateが失敗したとき

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,12 +29,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      flash.now[:alert] = "投稿に失敗しました。"
+      # flash.now[:alert] = "投稿に失敗しました。"
       render :edit
     end
   end
