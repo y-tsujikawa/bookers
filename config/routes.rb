9f9795fa501488b96dacf256c1405b9f4e46f73a
y-tsujikawa <184855547+y-tsujikawa@users.noreply.github.com>
2024-11-01 10:23:35 +0000

    [Add] bookのeditページの作成、showページにeditとbackのリンクを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'books' => 'books#new'
-  get 'books' => 'books#index'
   # get 'books' => 'books#new'
+  get 'books' => 'books#index'
+  get 'books' => 'books#new'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
-  get 'books/edit'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
 
   get '/' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
